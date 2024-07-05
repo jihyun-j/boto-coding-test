e8aca57e154ebeae626fa3bd6d272b016e49e897
jihyun-j <jihyun9142@gmail.com>
2024-07-05 14:57:46 +0900

    카드를 클릭했을 때 인덱스 값 가져오기

--- src/components/CardFilp.tsx
+++ src/components/CardFilp.tsx
@@ -5,13 +5,20 @@ const CardFilp = () => {
     .fill(1)
     .map((num, index) => num + index);
 
+  const [cardIndex, setCardIndex] = useState<number | null>(null);
+
+  const handleFlipCard = (index: number) => {
+    setCardIndex(cardIndex === index ? null : index);
+  };
+
   return (
     <div className="flex gap-10">
       {imgArr.map((index) => {
         return (
           <div
             key={index}
-            className={`relative w-56 h-80 cursor-pointer rounded-md bg-indigo-700`}>
+            className={`relative w-56 h-80 cursor-pointer rounded-md bg-indigo-700`}
+            onClick={() => handleFlipCard(index)}>
             <div
               className={`absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%]`}>
               Click Me
