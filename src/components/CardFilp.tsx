fac6a23f42a15e249ace17695efb926b0b402ae6
jihyun-j <jihyun9142@gmail.com>
2024-07-05 15:48:21 +0900

    카드flip 완성

--- src/components/CardFilp.tsx
+++ src/components/CardFilp.tsx
@@ -17,19 +17,19 @@ const CardFilp = () => {
         return (
           <div
             key={index}
-            className={`relative w-56 h-80 cursor-pointer rounded-md bg-indigo-700 ${
+            className={`relative w-56 h-80 cursor-pointer rounded-md bg-indigo-700 transition ease-in-out duration-500 transform-style-3d backface-hidden ${
               cardIndex === index && "transform rotate-y-180"
             }`}
             onClick={() => handleFlipCard(index)}>
             <div
-              className={`absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%]`}>
+              className={`absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] backface-hidden text-white`}>
               Click Me
             </div>
 
             <img
               src={`../image${index}.jpg`}
               alt={`image${index}`}
-              className={`w-full h-full object-cover rounded-md`}
+              className={`w-full h-full object-cover rounded-md backface-hidden transform rotate-y-180`}
             />
           </div>
         );
