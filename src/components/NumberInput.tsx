d13be7df86be5eee71fe701be8c63336510ad587
jihyun-j <jihyun9142@gmail.com>
2024-07-05 23:46:51 +0900

    하나의 소수점만 허용되는 로직 추가

--- src/components/NumberInput.tsx
+++ src/components/NumberInput.tsx
@@ -5,6 +5,13 @@ const NumberInput = () => {
 
   const onChangeNumber = (e: React.ChangeEvent<HTMLInputElement>) => {
     let inputValue = e.target.value.replace(/[^0-9.]/g, "");
+    const decimal = inputValue.split(".");
+
+    if (decimal.length > 2) {
+      const removeSecondDecimal = decimal.slice(1).join("");
+
+      inputValue = decimal[0] + "." + removeSecondDecimal;
+    }
 
     setNumber(inputValue.replace(/\B(?=(\d{3})+(?!\d))/g, ","));
   };
